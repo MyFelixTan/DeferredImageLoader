c016a25291f00d57a956f62f36c68fbf9be0e78a
MyFelixTan <my.felix.tan@gmail.com>
2014-10-11 02:54:39 +0800

    :tada: Added .gitattributes & .gitignore files

---
+++ deferred.js
@@ -0,0 +1,31 @@
+(function () {
+
+    cDeferred = {
+        init: function () {
+            var me = this;
+            var loadImageIfVisible = function () {
+
+                $("img").each(function (index) {
+                    if (!$(this).hasClass("load") && me.isScrolledIntoView(this)) {
+                            $(this).addClass("load");
+                    }
+                });
+            };
+            $(window).scroll(loadImageIfVisible);
+            loadImageIfVisible();
+        },
+
+        isScrolledIntoView: function (elem)
+        {
+            var docViewTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+            var docViewBottom = docViewTop + $(window).height();
+
+            var elemTop = $(elem).offset().top;
+            var elemBottom = elemTop + $(elem).height();
+
+            return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+        }
+
+    };
+
+}).call(this);
